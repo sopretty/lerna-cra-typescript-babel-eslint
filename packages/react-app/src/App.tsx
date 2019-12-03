25f77dead9b715e60c919ba02ab4dd60d503683f
Mathieu Jeanmougin <m.jeanmougin@tadaweb.com>
2019-12-03 18:08:31 +0100

    fix(events)

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,34 +1,43 @@
 import * as React from 'react';
-import {HelloWorld} from '@sopretty/react-ui';
+// import {HelloWorld} from '@sopretty/react-ui';
 import './App.css';
-
-declare global {
-  namespace JSX {
-      interface IntrinsicElements {
-          'tdw-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      }
-  }
-}
+import '@webcomponents/webcomponentsjs/webcomponents-loader';
+import '@sopretty/lit-element/dist/index';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <HelloWorld>test</HelloWorld>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-        <tdw-button></tdw-button>
-      </header>
-    </div>
+  React.useEffect(() => {
+    const myElement = document.querySelector('tdw-button');
+    const test = (e: any) => {
+      console.log('onclick', e);
+    };
+    if (myElement) {
+      myElement.addEventListener('fdp', test);
+      return myElement.removeEventListener('fdp', test);
+    }
+  }, []);
+
+  return React.createElement(
+    'tdw-button',
+    {color: 'danger'},
+    'my first button',
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer">
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
